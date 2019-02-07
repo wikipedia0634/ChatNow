6b5fb175844529ccfe5a2f67436dd959ce02b9c8
ThucVuong <mrkempq@gmail.com>
2019-02-07 14:54:22 +0700

    Chat Text

--- app/src/main/java/com/example/peter/project2/Chat.java
+++ app/src/main/java/com/example/peter/project2/Chat.java
@@ -1,17 +1,25 @@
 package com.example.peter.project2;
 
+import android.annotation.SuppressLint;
 import android.media.Image;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.github.nkzawa.emitter.Emitter;
@@ -28,12 +36,32 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 public class Chat extends AppCompatActivity {
 
     Button btn_back_chat;
     ImageButton btn_voice;
+
+    private EditText textField;
+    private ImageButton sendButton;
+
+    public static final String TAG  = "Chat";
+    public static String uniqueId;
+
+    private String Username;
+
+    private Boolean hasConnection = false;
+
+    private ListView messageListView;
+    private MessageAdapter messageAdapter;
+
+    private Thread thread2;
+    private boolean startTyping = false;
+    private int time = 2;
 
     private MediaRecorder myRecorder;
     private MediaPlayer myPlayer;
@@ -42,9 +70,27 @@ public class Chat extends AppCompatActivity {
     private Socket mSocket;
     {
         try {
-            mSocket = IO.socket("http://192.168.31.192:3000");
+            mSocket = IO.socket("http://192.168.1.87:5000");
         } catch (URISyntaxException e) {}
     }
+
+
+    @SuppressLint("HandlerLeak")
+    Handler handler2=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.i(TAG, "handleMessage: typing stopped " + startTyping);
+            if(time == 0){
+                setTitle("SocketIO");
+                Log.i(TAG, "handleMessage: typing stopped time is " + time);
+                startTyping = false;
+                time = 2;
+            }
+
+        }
+    };
+
 
 
 
@@ -63,6 +109,7 @@ public class Chat extends AppCompatActivity {
         });
 
         btn_voice = findViewById(R.id.voice);
+
 
         btn_voice.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -88,8 +135,282 @@ public class Chat extends AppCompatActivity {
             }
         });
 
+        Username = getIntent().getStringExtra("username");
+
+        uniqueId = UUID.randomUUID().toString();
+        Log.i(TAG, "onCreate: " + uniqueId);
+
+        if(savedInstanceState != null){
+            hasConnection = savedInstanceState.getBoolean("hasConnection");
+        }
+
+        if(hasConnection){
+
+        }else {
+            mSocket.connect();
+            mSocket.on("connect user", onNewUser);
+            mSocket.on("chat message", onNewMessage);
+            mSocket.on("on typing", onTyping);
+
+            JSONObject userId = new JSONObject();
+            try {
+                userId.put("username", Username + " Connected");
+                mSocket.emit("connect user", userId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.i(TAG, "onCreate: " + hasConnection);
+        hasConnection = true;
+
+
+        Log.i(TAG, "onCreate: " + Username + " " + "Connected");
+
+        textField = findViewById(R.id.textField);
+        sendButton = findViewById(R.id.sendButton);
+        messageListView = findViewById(R.id.messageListView);
+
+        List<MessageFormat> messageFormatList = new ArrayList<>();
+        messageAdapter = new MessageAdapter(this, R.layout.item_message, messageFormatList);
+        messageListView.setAdapter(messageAdapter);
+
+        onTypeButtonEnable();
+    }
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("hasConnection", hasConnection);
+    }
+
+    public void onTypeButtonEnable(){
+        textField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                JSONObject onTyping = new JSONObject();
+                try {
+                    onTyping.put("typing", true);
+                    onTyping.put("username", Username);
+                    onTyping.put("uniqueId", uniqueId);
+                    mSocket.emit("on typing", onTyping);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                if (charSequence.toString().trim().length() > 0) {
+                    sendButton.setEnabled(true);
+                } else {
+                    sendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+
+
+                    Log.i(TAG, "run: ");
+                    Log.i(TAG, "run: " + args.length);
+                    JSONObject data = (JSONObject) args[0];
+
+//                    byte[] audio;
+//                    try {
+//                        audio = (byte[]) data.get("noidung");
+//                        //thêm nút nhấn
+//                        playMp3FromByte(audio);
+//                    } catch (JSONException e) {
+//                        return;
+//                    }
+
+                    String username;
+                    String message;
+                    String id;
+                    byte[] audio;
+                    try {
+                        username = data.getString("username");
+                        message = data.getString("message");
+                        id = data.getString("uniqueId");
+
+                        Log.i(TAG, "run: " + username + message + id);
+
+                        MessageFormat format = new MessageFormat(id, username, message);
+                        Log.i(TAG, "run:4 ");
+                        messageAdapter.add(format);
+                        Log.i(TAG, "run:5 ");
+
+                        audio = (byte[]) data.get("noidung");
+                        //thêm nút nhấn
+                        playMp3FromByte(audio);
+
+                    } catch (Exception e) {
+                        return;
+                    }
+                }
+            });
+        }
+
+
+
+    };
+
+    Emitter.Listener onNewUser = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    int length = args.length;
+
+                    if(length == 0){
+                        return;
+                    }
+                    //Here i'm getting weird error..................///////run :1 and run: 0
+                    Log.i(TAG, "run: ");
+                    Log.i(TAG, "run: " + args.length);
+                    String username =args[0].toString();
+                    try {
+                        JSONObject object = new JSONObject(username);
+                        username = object.getString("username");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    MessageFormat format = new MessageFormat(null, username, null);
+                    messageAdapter.add(format);
+                    messageListView.smoothScrollToPosition(0);
+                    messageListView.scrollTo(0, messageAdapter.getCount()-1);
+                    Log.i(TAG, "run: " + username);
+                }
+            });
+        }
+    };
+
+
+    Emitter.Listener onTyping = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    JSONObject data = (JSONObject) args[0];
+                    Log.i(TAG, "run: " + args[0]);
+                    try {
+                        Boolean typingOrNot = data.getBoolean("typing");
+                        String userName = data.getString("username") + " is Typing......";
+                        String id = data.getString("uniqueId");
+
+                        if(id.equals(uniqueId)){
+                            typingOrNot = false;
+                        }else {
+                            setTitle(userName);
+                        }
+
+                        if(typingOrNot){
+
+                            if(!startTyping){
+                                startTyping = true;
+                                thread2=new Thread(
+                                        new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                while(time > 0) {
+                                                    synchronized (this){
+                                                        try {
+                                                            wait(1000);
+                                                            Log.i(TAG, "run: typing " + time);
+                                                        } catch (InterruptedException e) {
+                                                            e.printStackTrace();
+                                                        }
+                                                        time--;
+                                                    }
+                                                    handler2.sendEmptyMessage(0);
+                                                }
+
+                                            }
+                                        }
+                                );
+                                thread2.start();
+                            }else {
+                                time = 2;
+                            }
+
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    };
+
+    private void addMessage(String username, String message) {
 
     }
+
+    public void sendMessage(View view){
+        Log.i(TAG, "sendMessage: ");
+        String message = textField.getText().toString().trim();
+        if(TextUtils.isEmpty(message)){
+            Log.i(TAG, "sendMessage:2 ");
+            return;
+        }
+        textField.setText("");
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("message", message);
+            jsonObject.put("username", Username);
+            jsonObject.put("uniqueId", uniqueId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.i(TAG, "sendMessage: 1"+ mSocket.emit("chat message", jsonObject));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(isFinishing()){
+            Log.i(TAG, "onDestroy: ");
+
+            JSONObject userId = new JSONObject();
+            try {
+                userId.put("username", Username + " DisConnected");
+                mSocket.emit("connect user", userId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            mSocket.disconnect();
+            mSocket.off("chat message", onNewMessage);
+            mSocket.off("connect user", onNewUser);
+            mSocket.off("on typing", onTyping);
+            Username = "";
+            messageAdapter.clear();
+        }else {
+            Log.i(TAG, "onDestroy: is rotating.....");
+        }
+
+    }
+
+
     public void ghi(View view){
         try {
 
@@ -144,28 +465,28 @@ public class Chat extends AppCompatActivity {
         return bytes;
     }
 
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject data = (JSONObject) args[0];
-                    byte[] audio;
-                    try {
-                        audio = (byte[]) data.get("noidung");
-                        //thêm nút nhấn
-                        playMp3FromByte(audio);
-                    } catch (JSONException e) {
-                        return;
-                    }
+//    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+//        @Override
+//        public void call(final Object... args) {
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    JSONObject data = (JSONObject) args[0];
+//                    byte[] audio;
+//                    try {
+//                        audio = (byte[]) data.get("noidung");
+//                        //thêm nút nhấn
+//                        playMp3FromByte(audio);
+//                    } catch (JSONException e) {
+//                        return;
+//                    }
 
 //                    // add the message to view
 //                    addMessage(username, message);
-                }
-            });
-        }
-    };
+//                }
+//            });
+//        }
+//    };
 
     private void playMp3FromByte(byte[] mp3SoundByteArray) {
         try {
---
+++ app/src/main/java/com/example/peter/project2/MessageAdapter.java
@@ -0,0 +1,63 @@
+package com.example.peter.project2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MessageAdapter extends ArrayAdapter<MessageFormat> {
+    public MessageAdapter(Context context, int resource, List<MessageFormat> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.i(Chat.TAG, "getView:");
+
+
+        MessageFormat message = getItem(position);
+
+        if(TextUtils.isEmpty(message.getMessage())){
+
+
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.user_connected, parent, false);
+
+            TextView messageText = convertView.findViewById(R.id.message_body);
+
+            Log.i(Chat.TAG, "getView: is empty ");
+            String userConnected = message.getUsername();
+            messageText.setText(userConnected);
+
+        }else if(message.getUniqueId().equals(Chat.uniqueId)){
+            Log.i(Chat.TAG, "getView: " + message.getUniqueId() + " " + Chat.uniqueId);
+
+
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.my_message, parent, false);
+            TextView messageText = convertView.findViewById(R.id.message_body);
+            messageText.setText(message.getMessage());
+
+        }else {
+            Log.i(Chat.TAG, "getView: is not empty");
+
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.their_message, parent, false);
+
+            TextView messageText = convertView.findViewById(R.id.message_body);
+            TextView usernameText = (TextView) convertView.findViewById(R.id.name);
+
+            messageText.setVisibility(View.VISIBLE);
+            usernameText.setVisibility(View.VISIBLE);
+
+            messageText.setText(message.getMessage());
+            usernameText.setText(message.getUsername());
+        }
+
+        return convertView;
+    }
+}
